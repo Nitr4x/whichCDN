2a8e3d537ee0337d1f07affd307aef3a6e31e2d1
lejeun_j <jean.lejeune@bssi.fr>
2017-02-14 09:44:47 +0100

    Adding Whois detection plugin

---
+++ plugins/WhoisDetection/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""
+Whois Detection plugin performs CDN detection by analyzing data returned by
+whois command's.
+"""
+
+from plugins.WhoisDetection.behaviors import detect
